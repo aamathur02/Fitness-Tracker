3480a1c50fd471717e93b98604b037fc84057ce1
Aayush Mathur <aamathur02@gmail.com>
2021-05-15 11:26:52 -0400

    added basic functionality to personal info page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MealInputForm from './components/MealInputForm'
+import PersonalInfo from './components/PersonalInfo'
 
 function App() {
   return (
     <div className="App">
       <MealInputForm />
+      <PersonalInfo />
     </div>
   );
 }
--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {firestore} from './firebase'
-import {doc, getDoc} from './firebase/firestore'
 
 const PersonalInfo = () => {
 
@@ -19,15 +18,35 @@ const PersonalInfo = () => {
         state: GOALS.MAINTAIN,
     });
 
-    const populateDate = () => {
-        const docRef = doc(firestore, "Personal Info", "Info");
-        const docValue =  getDoc(docRef);
+    
 
-        if (docValue.exists()) {
-            
-        }
+    useEffect(() => {
+            var docRef = firestore.collection("Personal Info").doc("Info");
 
+            docRef.get().then(function(doc) {
+            if (doc.exists) {
+                setAge(doc.get("age"));
+                setFirstName(doc.get("firstName"));
+                setLastName(doc.get("lastName"));
+                setGoal(doc.get("goal"));
+                
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+    }, []);
 
-    }
-
+    return (
+        <div className="personal-info-form">
+            <p> Age: {age}</p>
+            <p> First Name: {firstName}</p>
+            <button onClick = {() => setAge(10)}> Submit</button>
+           
+        </div>
+    )
 }
+
+export default PersonalInfo;
